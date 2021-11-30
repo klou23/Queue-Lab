6840862e91288b97126dacb3244e418568b6c0c6
Kevin Lou <58053969+klou23@users.noreply.github.com>
2021-11-30 08:15:30 -0700

    Getters and Setters

--- src/QueueNode.java
+++ src/QueueNode.java
@@ -1,7 +1,7 @@
 public class QueueNode <T>{
 
-    T data;
-    QueueNode <T> next;
+    private T data;
+    private QueueNode <T> next;
 
     public QueueNode(T data) {
         this.data = data;
@@ -9,6 +9,22 @@ public class QueueNode <T>{
 
     public QueueNode(T data, QueueNode<T> next) {
         this.data = data;
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public QueueNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(QueueNode<T> next) {
         this.next = next;
     }
 }
