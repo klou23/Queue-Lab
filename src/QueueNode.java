bed2f08e174f65ec96a93f8112c2888f1f60cbf0
Kevin Lou <58053969+klou23@users.noreply.github.com>
2021-11-30 08:35:13 -0700

    javadoc comments for QueueNode

--- src/QueueNode.java
+++ src/QueueNode.java
@@ -3,27 +3,45 @@ public class QueueNode <T>{
     private T data;
     private QueueNode <T> next;
 
+    /**
+     * Constructor
+     */
     public QueueNode(T data) {
         this.data = data;
     }
 
+    /**
+     * Constructor
+     */
     public QueueNode(T data, QueueNode<T> next) {
         this.data = data;
         this.next = next;
     }
 
+    /**
+     * Getter for data
+     */
     public T getData() {
         return data;
     }
 
+    /**
+     * Setter for data
+     */
     public void setData(T data) {
         this.data = data;
     }
 
+    /**
+     * Getter for next
+     */
     public QueueNode<T> getNext() {
         return next;
     }
 
+    /**
+     * Setter for next
+     */
     public void setNext(QueueNode<T> next) {
         this.next = next;
     }
